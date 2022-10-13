d0188eeb898b2fa9171ff66da143ad928cbad460
Luis B <luisbttf@gmail.com>
2022-10-13 12:54:12 -0500

    newsletter

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -17,8 +17,7 @@ module.exports = {
   {
     resolve: "gatsby-plugin-mailchimp",
     options: {
-      endpoint:
-        "https://dreamx.us20.list-manage.com/subscribe/post?u=11a03b5699119a55c0e253226&amp;id=cc04537fdd", // string; add your MC list endpoint here; see instructions below
+      endpoint: "https://dreamx.us20.list-manage.com/subscribe/post?u=11a03b5699119a55c0e253226&amp;id=cc04537fdd", // string; add your MC list endpoint here; see instructions below
       timeout: 3500, // number; the amount of time, in milliseconds, that you want to allow mailchimp to respond to your request before timing out. defaults to 3500
     },
   }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
